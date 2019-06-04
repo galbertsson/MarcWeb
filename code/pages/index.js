031dafc60c4add7cc61fe980b6710be2ffb901aa
galbertsson <gustav.albertsson@hotmail.com>
2019-06-04 15:16:06 +0200

    Initial setup of nextjs and react

---
+++ code/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <style jsx>{`
+      .style{
+        color: green;
+      }
+      `}
+    </style>
+
+  <div className='style'>
+    Marc
+  </div>
+  </>
+);
