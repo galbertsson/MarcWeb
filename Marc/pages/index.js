7576fd38ec3c35d38ea47698efbc75210d55bc41
galbertsson <gustav.albertsson@hotmail.com>
2019-07-18 21:46:03 +0200

    Added basic firebase authentication
    There is still no persistence on the client side to remember that we are actually logged in

--- Marc/pages/index.js
+++ Marc/pages/index.js
@@ -1,10 +1,41 @@
-export default () => (
-  <div>
+;
+import firebase from 'firebase';
+import config from '../util/firebaseAuth'
+import { withRouter } from 'next/router'
+import dynamic from 'next/dynamic';
+
+//This is needed to disable SSR on StyledFirebaseAuth which does not support it
+const StyledFirebaseAuthNoSSR = dynamic(() => import("react-firebaseui/StyledFirebaseAuth"), { ssr: false });
+
+class Index extends React.Component {
+  constructor(props){
+    super(props)
+
+    if(!firebase.apps.length){
+      firebase.initializeApp(config)
+    }
+  }
+
+  uiConfig = {
+    signInFlow: 'popup',
+    signInSuccessUrl: '/decks',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  }
+
+  render(){
+    return <div>
     <style jsx>{`
       p {
         color: red;
       }
     `}</style>
     <p>Hello World!</p>
+
+    <StyledFirebaseAuthNoSSR uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
   </div>
-)
+  }
+}
+export default withRouter(Index);
---
+++ Marc/util/firebaseAuth.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyAsCLYUhRdm_bvLOGgwdVXKGcCSlNX7zO8",
+    authDomain: "marcapipoint.firebaseapp.com",
+    databaseURL: "https://marcapipoint.firebaseio.com",
+    projectId: "marcapipoint",
+    storageBucket: "",
+    messagingSenderId: "591562006117",
+    appId: "1:591562006117:web:e53bb13ecf0aad59"
+}
+export default config
