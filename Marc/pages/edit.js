49b576cebba8045247f65c37726641a063daf08c
galbertsson <gustav.albertsson@hotmail.com>
2019-07-29 22:53:29 +0200

    Added Firebase token when submitting an edited deck

--- Marc/pages/edit.js
+++ Marc/pages/edit.js
@@ -16,17 +16,21 @@ class Edit extends React.Component {
      * @param {string} title The title of the deck being created
      * @param {Array} notes The notes in the deck
      */
-    callback = (title, notes) => {
-        fetch("http://localhost:8080/decks/edit", {
-        method : 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body : JSON.stringify({
-            id : this.props.data.id,
-            title : title,
-            notes : notes})
-        }).then(() => console.log("Got it!"))
+    callback(title, notes){
+        this.props.user.getIdToken()
+        .then((token) => 
+            fetch("http://localhost:8080/decks/edit", {
+            method : 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body : JSON.stringify({
+                id : this.props.data.id,
+                title : title,
+                notes : notes})
+            }))
+        .then(() => console.log("Got it!"))
     }
 
     render(){
@@ -45,7 +49,7 @@ class Edit extends React.Component {
     }
 
     static async getInitialProps(context){
-        const res = await fetch(`http://localhost:8080/decks/${context.query.id}`);
+        const res = await fetch(`http://localhost:8080/decks/${context.query.id}`); //TODO: SEND TOKEN HERE, CANNOT DO THIS ON SERVER ANYMORE
         const data = await res.json();
       
         return {
