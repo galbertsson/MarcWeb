745242367ae71d50d2979592691be12b7f772167
galbertsson <gustav.albertsson@hotmail.com>
2019-07-29 22:52:54 +0200

    Added Firebase token when creating a deck

--- Marc/pages/create.js
+++ Marc/pages/create.js
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import DeckCreationContainer from '../components/creation/deckCreationContainer';
 
+export default class Create extends Component {
 
+constructor(data){
+  super(data)
 
-export default () => (
-  <>
-    <style jsx>{`
-      .style{
-        color: green;
-      }
-      `}
-    </style>
-
-  <div>
-      <DeckCreationContainer callback={callback}/>
-  </div>
-  </>
-);
+  this.callback = this.callback.bind(this)
+}
 
 /**
  * @param {string} title The title of the deck being created
  * @param {Array} notes The notes in the deck
  */
-const callback = (title, notes) => {
+callback(title, notes){
+  this.props.user.getIdToken().then((token) => 
   fetch("http://localhost:8080/decks/create", {
     method : 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     },
     body: JSON.stringify({
       title : title,
       notes : notes
     })
-  }).then(() => console.log("Got it!"))
+  }))
+  .then(() => console.log("Got it!"))
+}
+  
+  render() {
+    return (
+      <>
+      <style jsx>{`
+        .style{
+          color: green;
+        }
+        `}
+      </style>
+  
+    <div>
+        <DeckCreationContainer callback={this.callback}/>
+    </div>
+    </>
+    )
+  }
 }
