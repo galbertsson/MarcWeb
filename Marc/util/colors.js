9b6c6b9cbc1ec8043216cf3d78555e6839afe05c
galbertsson <gustav.albertsson@hotmail.com>
2019-08-13 22:21:04 +0200

    Added common colors

---
+++ Marc/util/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    primaryColor:"#00838F",
+    backgroundColor: "#E1E2E1",
+    textBackground: "#FFFFFF",
+    textColorPrimary:"#FFFFFF",
+    textColorSecondary:"#000000"
+}
+
+export default colors
