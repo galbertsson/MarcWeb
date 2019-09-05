fc61ba6df72b9308b5f1908920993b3017467e4a
galbertsson <gustav.albertsson@hotmail.com>
2019-09-05 17:47:01 +0200

    Made the login form as a full screen popup

--- Marc/components/auth/signin.js
+++ Marc/components/auth/signin.js
@@ -2,11 +2,34 @@ import React from 'react'
 
 const SignIn = ({onChange, submit}) => {
     return (
-        <form>
-            <input type="email" name="email" onChange={onChange} />
-            <input type="password" name="password" onChange={onChange} />
-            <button onClick={submit}>Login</button>
-        </form>
+        <div className={'login'}>
+            <style jsx>{`
+                .login{
+                    /*width: 500px;
+                    height: 200px;*/
+                    width: 100vw;
+                    height: 100vh;
+                    position: fixed;
+                    top: 0;
+                    background-color: rgba(0,0,0,0.25);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .form{
+                    width: 500px;
+                    height: 250px;
+                    background-color: white;
+                    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                }
+            `}</style>
+            <form className={'form'}>
+                <h1>Login</h1>
+                <input type="email" name="email" onChange={onChange} />
+                <input type="password" name="password" onChange={onChange} />
+                <button onClick={submit}>Login</button>
+            </form>
+        </div>
     )
 }
 
