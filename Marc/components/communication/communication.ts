7ab21cdb9ee8cd99e957fbcbcbbdbdaac3b4a47e
galbertsson <gustav.albertsson@hotmail.com>
2020-05-29 22:18:31 +0200

    Added superagent

--- Marc/components/communication/communication.ts
+++ Marc/components/communication/communication.ts
@@ -1,10 +1,12 @@
-
+import superAgent from 'superagent';
+import { API_URL } from '../../settings/setting';
 
 const sendPost = (path: string, body: any): Promise<any> => {
     const csrfToken = window.localStorage.getItem('csrf_token');
 
     if (!csrfToken) {
-        
+        superAgent
+            .get(`${API_URL}`)
         // TODO: Fetch csrf token
     }
 
